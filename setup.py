35eddba62d613433ab3eac7e5301c8c54f996d2c
Matt Einhorn <matt@einhorn.dev>
2022-03-18 23:08:35 -0400

    Create wheels with CIBW (#1)
    * Switch to using cbiw.
    
    * Switch to build for sdist.
    
    * Don't run on PR and exclude muslinux.

--- setup.py
+++ setup.py
@@ -118,9 +118,6 @@ ext_modules = [Extension(
 for e in ext_modules:
     e.cython_directives = {"embedsignature": True, 'language_level': 3}
 
-setup_requires = []
-if declare_cython:
-    setup_requires.append('cython')
 
 setup(
     name='kivy_garden.collider',
@@ -144,8 +141,7 @@ setup(
     keywords='Kivy kivy-garden',
 
     packages=find_namespace_packages(include=['kivy_garden.*']),
-    setup_requires=setup_requires,
-    install_requires=[],
+    install_requires=['kivy'],
     extras_require={
         'dev': ['pytest>=3.6', 'wheel', 'pytest-cov', 'pytest-asyncio',
                 'sphinx_rtd_theme'],
